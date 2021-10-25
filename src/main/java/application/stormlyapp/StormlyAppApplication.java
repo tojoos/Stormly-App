9a0f203fe20e6343be3d3f57554368adabb773e1
ToJoos <joosx2000@gmail.com>
2021-10-25 13:43:25 +0200

    Initial commit

---
+++ src/main/java/application/stormlyapp/StormlyAppApplication.java
@@ -0,0 +1,12 @@
+package application.stormlyapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StormlyAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StormlyAppApplication.class, args);
+    }
+}
---
+++ src/test/java/application/stormlyapp/StormlyAppApplicationTests.java
@@ -0,0 +1,13 @@
+package application.stormlyapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StormlyAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
