744c2fa6bbedbcce4b63f453485d228872d8a6f8
ToJoos <joosx2000@gmail.com>
2021-10-29 15:56:51 +0200

    Added extra queries in Record Repository to search for particular records. #19

--- src/main/java/application/stormlyapp/repositories/RecordRepository.java
+++ src/main/java/application/stormlyapp/repositories/RecordRepository.java
@@ -3,5 +3,10 @@ package application.stormlyapp.repositories;
 import application.stormlyapp.model.Record;
 import org.springframework.data.repository.CrudRepository;
 
+import java.time.LocalDateTime;
+import java.util.Optional;
+
 public interface RecordRepository extends CrudRepository<Record, Long> {
+    Optional<Record> findRecordByDate(LocalDateTime date);
+    Optional<Record> findAllByDate(LocalDateTime date);
 }
