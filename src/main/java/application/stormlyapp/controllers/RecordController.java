88be588f2d25112134c369e043513a3e7ce5a9df
ToJoos <joosx2000@gmail.com>
2021-10-31 19:34:13 +0100

    Added timestamp search on archive page. Closes #19.

--- src/main/java/application/stormlyapp/controllers/RecordController.java
+++ src/main/java/application/stormlyapp/controllers/RecordController.java
@@ -5,10 +5,10 @@ import application.stormlyapp.services.RecordService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.time.temporal.ChronoUnit;
+import java.util.HashSet;
 import java.util.Set;
 
 @Controller
@@ -28,6 +28,28 @@ public class RecordController {
     @GetMapping("/archive")
     public String getArchivePage(Model model) {
         model.addAttribute("records", recordService.findAll());
+        return "archivePage";
+    }
+
+    @GetMapping("/archive/{timeStamp}")
+    public String getArchivePageDay(Model model, @PathVariable String timeStamp) {
+        ChronoUnit unit;
+
+        switch(timeStamp) {
+            case "Day":
+                unit = ChronoUnit.DAYS;
+                break;
+            case "Week":
+                unit = ChronoUnit.WEEKS;
+                break;
+            case "Month":
+                unit = ChronoUnit.MONTHS;
+                break;
+            default:
+                return "404Page";
+        }
+
+        model.addAttribute("records", recordService.findAllBeforeDate(1L, unit));
         return "archivePage";
     }
 }
