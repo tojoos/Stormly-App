0e2b795f5f8f3bb1ab2959f550b7231a38033527
ToJoos <joosx2000@gmail.com>
2021-10-31 20:19:43 +0100

    Added User POJO. Closes #26.

---
+++ src/main/java/application/stormlyapp/model/User.java
@@ -0,0 +1,28 @@
+package application.stormlyapp.model;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Setter
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "login")
+    private String login;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "email")
+    private String email;
+}
