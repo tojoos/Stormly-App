6fc14ece5e4a51c7f8bd6d5159c23e2e49a3f1fd
ToJoos <joosx2000@gmail.com>
2021-11-26 19:25:30 +0100

    Added clock to main page. Closes #47

---
+++ src/main/resources/static/js/clock.js
@@ -0,0 +1,25 @@
+var inc = 1000;
+
+clock();
+
+function clock() {
+    const date = new Date();
+
+    const hours = ((date.getHours() + 23) % 24 + 1);
+    const minutes = date.getMinutes();
+    const seconds = date.getSeconds();
+
+    const hour = hours * 30;
+    const minute = minutes * 6;
+    const second = seconds * 6;
+
+    document.querySelector('.hour').style.transform = `rotate(${hour}deg)`
+    document.querySelector('.minute').style.transform = `rotate(${minute}deg)`
+    document.querySelector('.second').style.transform = `rotate(${second}deg)`
+
+    $(".hours").html(( hours < 10 ? "0" : "" ) + hours);
+    $(".min").html(( minutes < 10 ? "0" : "" ) + minutes);
+    $(".sec").html(( seconds < 10 ? "0" : "" ) + seconds);
+}
+
+setInterval(clock, inc);
